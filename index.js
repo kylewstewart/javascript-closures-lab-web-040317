f89978769346e68a81e3eabfce390fcd4aa15d84
kylewstewart <kyle.w.stewart@mac.com>
2017-05-17 15:41:39 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump() {
+    counter += 1
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {addBump, getBumps};
+}
+
+function createAnimal(animalType) {
+     return (deadlyDevice) => {
+       return {animalType, deadlyDevice}
+    }
+}
+
+var sharkCreator = createAnimal("shark")
+
+var sharkWithFrickinLaserbeam = createAnimal("Shark")("Laserbeam")
+var sharkWithFrickinCannon = createAnimal("Shark")("Cannon")
+
+ // function createAnimal(animalType) {
+ //
+ //   var deadlyDevice = null
+ //
+ //   function deadlyDevice () {}
+ //
+ //   return {animalType, deadlyDevice}
+ //
+ // }
